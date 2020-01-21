38dcde4d4c137c97850f4bc6cb4ccccb3850be19
JhersonCrack <metalero.jherson.zl@gmail.com>
2020-01-21 00:21:59 -0500

    Modal creado satisfactoriamente

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,10 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+/* Aquí irán todos los componentes creados */
+
+Vue.component('card-modal', require('./components/CardModal.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -29,4 +33,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    data: {
+        exampleModalShowing: false,
+    }
 });
